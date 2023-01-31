ecca04362789d8c4d2c4b5010113c88521a024ff
°Riccardo <52591461+Riccardocode@users.noreply.github.com>
2023-01-31 16:23:45 +0100

    eliminato style2

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,37 @@
-var xlsx = require("xlsx");
-var dataPathExcel = "database.xlsx";
-var wb;
-var sheeName;
-var sheeValue;
-var excelData;
-const path = require('path');
-const file = path.join(__dirname, 'data.xlsx');
-$(function(){
-    $('#load-data').click(function(){
-        
-        wb=xlsx.readFile(dataPathExcel);
-        sheeName = wb.SheetNames[0];
-        sheeValue = wb.Sheets[sheeName];
-        //console.log(sheeValue);
 
-        excelData = xlsx.utils.sheet_to_json(sheeValue);
-        console.log(excelData);
-    });
+const users=[
+    [1, 'Michael', 'Smith', 'Resource'],
+    [2,	'Jennifer', 'Johnson', 'Resource'],
+    [3,	'Jessica', 'Williams',	'Resource'],
+    [4,	'David', 'Jones','General Manager'],
+    [5,	'James', 'Brown', 'Resource'],
+    [6,	'Christopher', 'Garcia', 'Resource'],
+    [7,	'Matthew', 'Miller', 'Project manager'],
+    [8,	'Joshua', 'Davis', 'Project manager'],
+    [9,	'Ashley', 'Rodriguez', 'Project manager'],
+    [10,'Sarah', 'Martinez', 'Project manager'],
+    [11,'Stephanie', 'Hernandez', 'Project manager'],
+    [12,'Melissa', 'Lopez', 'Resource'],
+    [13,'Nicholas', 'Gonzalez', 'Resource'],
+    [14,'Amanda', 'Wilson', 'Resource'],
+    [15,'Andrew', 'Anderson', 'Resource'],
+    [16,'Brandon', 'Thomas', 'Resource'],
+    [17,'Stephanie', 'Taylor', 'Resource'],
+    [18,'Kelly', 'Moore', 'Resource'],
+    [19,'Daniel', 'Jackson', 'Resource'],
+    [20,'Megan', 'Martin', 'Resource']
+]
+const btn = document.getElementById("btnusers");
+
+
+
+
+
+
+btn.addEventListener("click",function(){
+  alert(users);
 });
+
 
 $(function(){
     $('#loginText').click(function(){
